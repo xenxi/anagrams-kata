29443cef15ae7f1477bd58b94b55e238fbda08d7
andiaz <andiaz@aidacanarias.com>
2021-09-01 11:00:17 +0200

    add test: have_3_words

--- AnagramsKataDotnet/AnagramsKata.Tests/FileDictionaryWordValidatorShould.cs
+++ AnagramsKataDotnet/AnagramsKata.Tests/FileDictionaryWordValidatorShould.cs
@@ -8,13 +8,23 @@ namespace AnagramsKata.Tests
     public class FileDictionaryWordValidatorShould
     {
         [Test]
-        public void have_370103_words()
+        public void have_4_words()
         {
-            var englishFileDictionaryWordValidator = new FileDictionaryWordValidator();
+            var englishFileDictionaryWordValidator = new FileDictionaryWordValidator("a\nb\nc\nd");
 
             var totalWords = englishFileDictionaryWordValidator.Count();
 
-            totalWords.Should().Be(370103);
+            totalWords.Should().Be(4);
+        }
+
+        [Test]
+        public void have_3_words()
+        {
+            var englishFileDictionaryWordValidator = new FileDictionaryWordValidator("a\nb\nc");
+
+            var totalWords = englishFileDictionaryWordValidator.Count();
+
+            totalWords.Should().Be(3);
         }
     }
 }
--- AnagramsKataDotnet/AnagramsKata/FileDictionaryWordValidator.cs
+++ AnagramsKataDotnet/AnagramsKata/FileDictionaryWordValidator.cs
@@ -1,10 +1,27 @@
-﻿namespace AnagramsKata
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace AnagramsKata
 {
     public class FileDictionaryWordValidator
     {
+        private readonly Dictionary<int, List<string>> _wordDictionary;
+
+        public FileDictionaryWordValidator(string data)
+        {
+            _wordDictionary = ReadWords(data);
+        }
+
+        private Dictionary<int, List<string>> ReadWords(string data)
+        {
+            var words = data.Split('\n');
+
+            return words.GroupBy(x => x.Length).ToDictionary(x => x.Key, x => x.ToList());
+        }
+
         public int Count()
         {
-            return 370103;
+            return _wordDictionary.Values.Sum(x => x.Count);
         }
     }
 }
