c64d230568801797d533be810b2eaf7e09c1e201
andiaz <andiaz@aidacanarias.com>
2021-09-01 08:44:27 +0200

    add test: have_370103_words

---
+++ AnagramsKataDotnet/AnagramsKata.Tests/FileDictionaryWordValidatorShould.cs
@@ -0,0 +1,20 @@
+﻿using System.Security.Cryptography.X509Certificates;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace AnagramsKata.Tests
+{
+    [TestFixture]
+    public class FileDictionaryWordValidatorShould
+    {
+        [Test]
+        public void have_370103_words()
+        {
+            var englishFileDictionaryWordValidator = new FileDictionaryWordValidator();
+
+            var totalWords = englishFileDictionaryWordValidator.Count();
+
+            totalWords.Should().Be(370103);
+        }
+    }
+}
---
+++ AnagramsKataDotnet/AnagramsKata/FileDictionaryWordValidator.cs
@@ -0,0 +1,10 @@
+﻿namespace AnagramsKata
+{
+    public class FileDictionaryWordValidator
+    {
+        public int Count()
+        {
+            return 370103;
+        }
+    }
+}
